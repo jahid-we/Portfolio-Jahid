7bc5ee198b5095cafa9c221bc660ef24102a26a1
jahid-we <jahidhasan370919@gmail.com>
2025-07-17 23:51:17 +0600

    Route Fixed

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,28 @@
 <?php
 
+use App\Http\Controllers\Admin\AuthenticationController;
+use App\Http\Controllers\Admin\ResetPasswordController;
 use App\Http\Controllers\Pages\PageController;
 use Illuminate\Support\Facades\Route;
 
 // Route::get('/', [PageController::class, 'indexPage']);
+
+// Authentication Route Start***********************************************
+Route::post('/login', ['AuthenticationController', 'login'])->middleware('guest')->name('login');
+
+Route::middleware('auth')->controller(AuthenticationController::class)->group(function () {
+
+    Route::post('/logout', 'logout')->name('logout');
+
+});
+// Authentication Route End*************************************************
+
+// Reset Password Route Start********************************
+Route::middleware('guest')->controller(ResetPasswordController::class)->group(function () {
+    Route::post('/send-email', 'sendEmail')->name('sendEmail');
+    Route::post('/reset-password', 'resetPassword')->name('resetPassword');
+});
+// Reset Password Route End********************************
 
 //  Public Page Routes Start ***************************************************************
 Route::controller(PageController::class)->group(function () {
